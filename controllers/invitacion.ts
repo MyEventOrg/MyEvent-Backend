591a2a178469dfd096deb0e1364232e659d25b92
ArtSilP <silveraarturo199@gmail.com>
2025-11-21 19:59:41 -0500

    fix rama juan

--- controllers/invitacion.ts
+++ controllers/invitacion.ts
@@ -98,8 +98,8 @@ class InvitacionController {
                     mensaje: `${usuarioSolicitante.nombreCompleto} solicita unirse al evento.`,
                     tipo: "solicitud",  // ← Marca como solicitud
                     fecha_invitacion: new Date(),
-                    organizador_id: usuario_id,  // ← El que solicita
-                    invitado_id: organizador.usuario_id,  // ← El organizador
+                    organizador_id: organizador.usuario_id,
+                    invitado_id: usuario_id,
                     evento_id
                 });
 
